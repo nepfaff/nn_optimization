61e4852fa6713502625a2127475879f084abc945
Nicholas Pfaff <contact@nicholaspfaff.ch>
2023-04-14 09:36:59 -0400

    Simplify MLP-9

--- nn_optimization/models/mlp9.py
+++ nn_optimization/models/mlp9.py
@@ -5,73 +5,38 @@ import torch.nn as nn
 class MLP9(nn.Module):
     def __init__(
         self,
-        use_dropout: bool,
-        dropout_prob: float,
         in_ch: int,
         out_ch: int,
         hidden_ch: int,
     ):
         super().__init__()
 
-        print(
-            f"[MLP-9] Dropout: {use_dropout}; Do_prob: {dropout_prob}; in_ch: {in_ch}; hidden_ch: {hidden_ch}"
-        )
+        print(f"[MLP-9] in_ch: {in_ch}; hidden_ch: {hidden_ch}")
 
         self.latent = nn.Parameter(torch.rand((1, in_ch)))
-        if use_dropout is False:
-            self.net1 = nn.Sequential(
-                nn.utils.weight_norm(nn.Linear(in_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch - in_ch)),
-                nn.ReLU(inplace=True),
-            )
 
-            self.net2 = nn.Sequential(
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.Linear(hidden_ch, out_ch),
-            )
-        else:
-            self.net1 = nn.Sequential(
-                nn.utils.weight_norm(nn.Linear(in_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout_prob, inplace=False),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout_prob, inplace=False),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout_prob, inplace=False),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch - in_ch)),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout_prob, inplace=False),
-            )
+        self.net1 = nn.Sequential(
+            nn.Linear(in_ch, hidden_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_ch, hidden_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_ch, hidden_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_ch, hidden_ch - in_ch),
+            nn.ReLU(inplace=True),
+        )
 
-            self.net2 = nn.Sequential(
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout_prob, inplace=False),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout_prob, inplace=False),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout_prob, inplace=False),
-                nn.utils.weight_norm(nn.Linear(hidden_ch, hidden_ch)),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout_prob, inplace=False),
-                nn.Linear(hidden_ch, out_ch),
-            )
+        self.net2 = nn.Sequential(
+            nn.Linear(hidden_ch, hidden_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_ch, hidden_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_ch, hidden_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_ch, hidden_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_ch, out_ch),
+        )
 
         num_params = sum(p.numel() for p in self.parameters())
         print("[num parameters: {}]".format(num_params))
